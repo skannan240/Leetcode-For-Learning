29ba482e50f9e72abbea26cc6c3e699842827fde
skannan240 <shyamkannan2000@gmail.com>
2022-07-27 16:23:06 -0700

    Time: 49 ms (90.98%), Space: 14.6 MB (42.81%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,7 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         newS = ''.join(filter(str.isalnum, s))
-        newS.lower()
         if newS[::-1].lower() == newS.lower():
             return True
         else:
