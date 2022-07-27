24f6e74595af740b9fbed20adce6d27c33e4eb8b
skannan240 <shyamkannan2000@gmail.com>
2022-07-27 16:10:12 -0700

    Time: 39 ms (75.66%), Space: 13.9 MB (70.03%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        closeToOpen = { ")" : "(", "]" : "[", "}" : "{"}
+        
+        
+        
+        for c in s:
+            #check if character is closing parenthesis
+            if c in closeToOpen:
+                #stack[-1] is llast value we added to stack
+                if stack and stack[-1] == closeToOpen[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        
+        #only return true if stack is empty
+        return True if not stack else False
+            
